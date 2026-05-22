cda2f3f3af666d467cabdb76f18a8a43250e5997
OscarGG <oscar.email>
2026-05-21 22:02:22 -0500

    Configuracion inicial de librerias ADC y variables globales

--- main/main.c
+++ main/main.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gptimer.h"
+#include "esp_adc/adc_oneshot.h"
 
+adc_oneshot_unit_handle_t adc_handle;
+volatile bool leer_adc = false;
