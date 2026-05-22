cc6b346d75137d62d060c921fec4bc397f44e4a0
OscarGG <oscar.email>
2026-05-21 20:48:51 -0500

    Configuracion inicial de librerias variables y GPIO

--- main/main.c
+++ main/main.c
@@ -3,3 +3,12 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+
+int leds[] = {2, 4, 5, 18, 19};
+
+#define NUM_LEDS 5
+#define BTN_DIRECCION 21
+#define BTN_PAUSA 22
+
+int direccion = 1;
+bool pausa_total = false; 
